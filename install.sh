5f7efd8af8ec7f5dbff7cb4b0694a24166eab5fa
Jorge Antônio da Silva <jorge@Jorges-MacBook-Pro.local>
2020-01-15 20:56:39 -0300

    Add custom binaries installs and change files configuration

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-# VIM
 echo "Symlinking things:"
 
 ln -s $(pwd)/.gitconfig ~/.gitconfig
@@ -16,14 +15,61 @@ brew cask install google-chrome
 echo "Installing VS Code"
 brew cask install visual-studio-code
 
-echo "Installing Obs"
-brew cask install obs
+echo "Installing 1Password"
+brew cask 1password
+
+echo "Installing iTerm2"
+brew cask iterm2
+
+echo "Installing Spotify"
+brew cask spotify
+
+echo "Installing Skype"
+brew cask skype
+
+echo "Installing Handbrake"
+brew cask handbrake
+
+echo "Installing Firefox"
+brew cask firefox
+
+echo "Installing Robo 3T"
+brew cask robo-3t
+
+echo "Installing Google Drive File Stream"
+brew cask google-drive-file-stream
+
+echo "Installing DBeaver"
+brew cask dbeaver-community
+
+echo "Installing Reactotron"
+brew cask reactotron
+
+echo "Installing IINA Player"
+brew cask iina
+
+echo "Installing Postman"
+brew cask postman
 
 echo "Installing AWS CLI"
 brew install awscli
 
 echo "Installing ffmpeg"
 brew install ffmpeg
+
+echo "Installing Watchman"
+brew install watchman
+
+echo "Installing Cocoapods"
+brew install cocoapods
+
+echo "Installing Java JDK"
+brew tap AdoptOpenJDK/openjdk
+brew cask install adoptopenjdk8
+
+echo "Installing FiraCode"
+brew tap caskroom/fonts
+brew cask install font-fira-code
 
 echo "Making Zsh the default shell"
 chsh -s $(which zsh)
